ba45cb36903bcfcd2154f3d697c967b4898e8515
Pedro Almeida Carvalho <cont.pacarvalho@gmail.com>
2024-05-16 13:09:54 -0300

    commit req 6

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,18 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
     """Aqui irá sua implementação"""
+    p_queue = PriorityQueue()
+    p_queue.enqueue({"qtd_linhas": 7})
+    p_queue.enqueue({"qtd_linhas": 3})
+    p_queue.enqueue({"qtd_linhas": 5})
+    p_queue.enqueue({"qtd_linhas": 1})
+    assert len(p_queue) == 4
+    assert p_queue.dequeue() == {"qtd_linhas": 3}
+    assert p_queue.search(2) == {"qtd_linhas": 5}
+    with pytest.raises(IndexError):
+        p_queue.search(len(p_queue) + 1)
+    with pytest.raises(IndexError):
+        p_queue.search(-1)
