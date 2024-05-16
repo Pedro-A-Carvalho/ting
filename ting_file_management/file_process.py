1d3838763eeb0038dab29b4692a8222df1df4af4
Pedro Almeida Carvalho <cont.pacarvalho@gmail.com>
2024-05-16 12:06:03 -0300

    commit req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return
+    data = txt_importer(path_file)
+    processed_data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(data),
+        "linhas_do_arquivo": data,
+    }
+    instance.enqueue(processed_data)
+    print(processed_data)
 
 
 def remove(instance):
