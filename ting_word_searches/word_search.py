180dfeaaa87f1ac33a5f6f3e15fe9a61749101af
Pedro Almeida Carvalho <cont.pacarvalho@gmail.com>
2024-05-16 13:59:50 -0300

    commit req 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,8 +1,5 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+def search_word(word, instance, content=False):
     data = []
-    # if not word:
-    #     return []
     for index in range(len(instance)):
         current = instance.search(index)
         current_data = {
@@ -10,13 +7,28 @@ def exists_word(word, instance):
             "arquivo": current["nome_do_arquivo"],
             "ocorrencias": [],
         }
-        for index_line, line in enumerate(current["linhas_do_arquivo"]):
-            if word.lower() in line.lower():
-                current_data["ocorrencias"].append({"linha": index_line + 1})
+        current_data["ocorrencias"] = word_in_lines(
+            word, current["linhas_do_arquivo"], content
+        )
         if current_data["ocorrencias"]:
             data.append(current_data)
     return data
 
 
+def word_in_lines(word, lines, content):
+    occourences = []
+    for index_line, line in enumerate(lines):
+        if word.lower() in line.lower():
+            occourence = {"linha": index_line + 1}
+            if content:
+                occourence["conteudo"] = line
+            occourences.append(occourence)
+    return occourences
+
+
+def exists_word(word, instance):
+    return search_word(word, instance)
+
+
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    return search_word(word, instance, True)
