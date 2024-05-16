b7bd31ea5e75eaf61d30c1a6282017dfd8b0a792
Pedro Almeida Carvalho <cont.pacarvalho@gmail.com>
2024-05-16 12:44:23 -0300

    commit req 5

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,3 +1,4 @@
+import sys
 from ting_file_management.file_management import txt_importer
 
 
@@ -28,3 +29,7 @@ def remove(instance):
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+    if position < 0 or position >= len(instance):
+        sys.stderr.write("Posição inválida\n")
+        return
+    print(instance.search(position))
