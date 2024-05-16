969b7bdbc772c8112148c51c640360ac03665f9e
Pedro Almeida Carvalho <cont.pacarvalho@gmail.com>
2024-05-16 11:35:37 -0300

    commit req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido\n")
+    try:
+        with open(path_file, "r") as file:
+            data = [line.strip() for line in file]
+        return data
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
